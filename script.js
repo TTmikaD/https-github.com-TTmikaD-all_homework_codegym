ada2c100ff5fc8f05c62bfa0ec493797b7a7d91d
TTmikaD <101580923+TTmikaD@users.noreply.github.com>
2023-05-22 20:36:32 +0700

    1 file modified

--- script.js
+++ script.js
@@ -200,8 +200,8 @@
 // alert('bullshit! thôi mày vào đi');
 
 // let x = [[1, 2, 3, 4, 5, 6, 7][('a', 'b', 'c', 'd', 'e')]];
-let x = [1, 2, 3, 4, 5];
+// let x = [1, 2, 3, 4, 5];
 
-for (let i = 0; i < x.length; i++) {
-  console.log('phần tử thứ:', i, '=', x[i]);
-}
+// for (let i = 0; i < x.length; i++) {
+//   console.log('phần tử thứ:', i, '=', x[i]);
+// }
